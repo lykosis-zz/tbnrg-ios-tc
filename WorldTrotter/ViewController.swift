82f66853fbd167d663853ad97f6f1cb2cfc374f9
Alejandro L. Hernandez <alejandro@ulfix.com>
2016-11-23 12:11:10 -0600

    Commit inicial

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Alejandro López Hernández on 19/11/16.
+//  Copyright © 2016 Alejandro López Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
